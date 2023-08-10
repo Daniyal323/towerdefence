bd07f96c03cf19724b584a61b63b40ee285eb4f7
DaniyalJawad <daniyaljawad77@gmail.com>
2023-08-10 17:52:42 +0500

    module3 animation added on player
    walking and idle animation has added with slight camera improvement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,12 @@ public class PlayerController : MonoBehaviour
     public float speed = 4f;
     float turnSmoothVelocity;
     public float turnSmoothTime = 0.1f;
+    Animator anim;
+
+    private void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -19,12 +25,16 @@ public class PlayerController : MonoBehaviour
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
         if(direction.magnitude >= 0.1f)
         {
+            anim.SetFloat("movement", 1);
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             Vector3 movDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(movDir.normalized * speed * Time.deltaTime);
         }
-
+        else if(direction.magnitude == 0)
+        {
+            anim.SetFloat("movement", 0);
+        }
     }
 }
