1b82a5f60711ed1d127195c349f9a83a0b369a88
Veonaary <veonary24@gmail.com>
2023-08-19 18:05:55 +0500

    module 9
    main menu

--- Assets/Scripts/WeaponSelection.cs
+++ Assets/Scripts/WeaponSelection.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class WeaponSelection : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+  
+
+    public int SelectedWeapon=0;
+     void Update()
+    {
+        SelectWeapon();
+    }
+    public void Weapon_One()
     {
         
+        SelectedWeapon = 0;
+    }  
+    public void Weapon_Two()
+    {
+        if (transform.childCount >= 2)
+        {
+            SelectedWeapon = 1;
+        }
+          
+    }
+    public void Weapon_Three()
+    {
+        if (transform.childCount >= 3)
+        {
+            SelectedWeapon = 2;
+        }
+    }
+    void SelectWeapon()
+    {
+        int i = 0;
+        foreach(Transform weapon in transform)
+        {
+            if (i == SelectedWeapon)
+                weapon.gameObject.SetActive(true);
+            else
+                weapon.gameObject.SetActive(false);
+            i++;
+        }
+
     }
 }
